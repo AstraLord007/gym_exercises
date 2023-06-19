1636772d83a1ffb656c76ef430bf2adcdfeb14e0
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2023-06-19 16:31:21 -0300

    ExerciseCard component with image gif.

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState} from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyParts }) => {
   return (
@@ -9,7 +10,7 @@ const Exercises = ({ exercises, setExercises, bodyParts }) => {
       <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
       <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
         {exercises.map((exercise, index) => (
-          <p>{exercise.name}</p>
+          <ExerciseCard ket={index} exercise={exercise} />
         ))}
       </Stack>
       {/* <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
